f1b62da063f05da35af1948ae38206953f635c8a
Ilja Khabarov <ilja.khabarov@gmail.com>
2022-10-06 15:58:01 +0700

    Move storage out of lib.rs; Some other refactoring too.

--- pallets/oracle/src/lib.rs
+++ pallets/oracle/src/lib.rs
@@ -1,132 +1,18 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+mod storage;
+
 pub use pallet::*;
-use frame_support::{inherent::Vec, pallet_prelude::*};
+use frame_support::pallet_prelude::*;
 use frame_system::pallet_prelude::*;
 
-use codec::{Encode, Decode};
 use pallet_timestamp::{self as timestamp};
 
-/// Timestamp format is intended to be as seconds(s)
-const HOUR_DURATION: u64 = 3_600;
-/// Let list size be bound by 1000. This will prevent us from putting too much data
-/// in blockchain storage.
-const LIST_SIZE: u64 = 1000;
-
-#[derive(Clone, Default, Encode, Decode, TypeInfo)]
-pub struct OracleEvent {
-	pub data: Vec<u8>,
-	/// just seconds
-	pub timestamp: u64,
-}
-
-/// List-like implementation.
-#[derive(Encode, Decode, TypeInfo)]
-pub struct OracleEventStorage {
-	pub events: Vec<OracleEvent>,
-	/// Current index of start
-	pub start: u64,
-	/// Current amount of valid events
-	pub size: u64,
-	/// Thought of doing some performance enhancement here.
-	#[deprecated]
-	pub last_timestamp: u64,
-}
-
-impl Default for OracleEventStorage {
-	fn default() -> Self {
-		Self::init_sized(LIST_SIZE)
-	}
-}
-impl OracleEventStorage {
-	pub fn init_sized(size: u64) -> Self {
-		let mut v = Vec::new();
-		v.reserve(size as usize);
-		for i in 0..size {
-			v.push(OracleEvent::default());
-		}
-		Self {
-			events: v,
-			start: 0,
-			size: 0,
-			last_timestamp: 0,
-		}
-	}
-	pub fn cleanup(&mut self, now: u64) {
-		let mut counter = self.get_start();
-		let mut cleaned_start = counter;
-		let vec_size = self.events.len();
-
-		for i in 0..self.size {
-			let ts = self.events[counter].timestamp;
-			if now - ts > HOUR_DURATION {
-				cleaned_start = (cleaned_start + 1) % vec_size;
-				self.size = self.size - 1;
-			} else {
-				break;
-			}
-			counter = counter + 1;
-		}
-		self.start = cleaned_start as u64;
-	}
-	pub fn add_event(&mut self, event: OracleEvent, now: u64) {
-		let put_idx = (self.get_start() + self.get_size()) % self.events.len();
-		self.events[put_idx] = event;
-		//self.idx = self.idx + 1;
-		if self.size < self.events.len() as u64 {
-			self.size = self.size + 1;
-		}
-	}
-	pub fn get_start(&self) -> usize {
-		self.start as usize
-	}
-	pub fn get_size(&self) -> usize {
-		self.size as usize
-	}
-}
-
-/// All-in-one to make it easier to run in IDE.
-#[test]
-fn test_storage() {
-	let mut storage = OracleEventStorage::default();
-	assert_eq!(1000, storage.events.len());
-
-	let event = OracleEvent {
-		data: vec![],
-		timestamp: 1000,
-	};
-	for i in 0..50 {
-		storage.add_event(event.clone(), 1000);
-	}
-	assert_eq!(50, storage.size);
-	storage.cleanup(1000 + HOUR_DURATION + 1);
-	assert_eq!(0, storage.size);
-	assert_eq!(50, storage.start);
-
-	let newer_event = OracleEvent {
-		data: vec![],
-		timestamp: 1100,
-	};
-
-	for i in 0..50 {
-		storage.add_event(event.clone(), 1000);
-	}
-	assert_eq!(50, storage.size);
-	for i in 0..50 {
-		storage.add_event(newer_event.clone(), 1100);
-	}
-	assert_eq!(100, storage.size);
-	storage.cleanup(1000 + HOUR_DURATION + 1);
-	assert_eq!(100, storage.start);
-	assert_eq!(50, storage.size);
-}
+use storage::{OracleEventStorage, RawEvent};
 
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-
-	pub type OracleEvent = Vec<u8>;
-	pub type StoredEvents = Vec<Vec<u8>>;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -140,12 +26,7 @@ pub mod pallet {
 	}
 
 	#[pallet::storage]
-	#[pallet::getter(fn oracle_events)]
-	pub type Events<T> = StorageValue<_, StoredEvents, ValueQuery>;
-
-
-	#[pallet::storage]
-	#[pallet::getter(fn foo)]
+	#[pallet::getter(fn event_storage)]
 	pub type OracleEventStorageStorage<T> = StorageValue<_, OracleEventStorage, ValueQuery>;
 
 	#[pallet::event]
@@ -153,16 +34,14 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		ReceivedEvent(u64),
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		/// WIP: temporary solution
+		SomeError,
 	}
 
 	#[pallet::call]
@@ -170,15 +49,22 @@ pub mod pallet {
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
 		pub fn handle_event(
 			origin: OriginFor<T>,
-			event: Vec<u8>,
+			event: RawEvent,
 		) -> DispatchResult {
-			let who = ensure_root(origin)?;
-			let mut events = <Events<T>>::get();
-			let _now = <timestamp::Pallet<T>>::get(); /// actually duration = {secs, nanosecs}
+			ensure_root(origin)?;
+			let mut events = <OracleEventStorageStorage<T>>::get();
 
-			events.push(event);
+			let now = <timestamp::Pallet<T>>::get();
+			let now = TryInto::<u64>::try_into(now)
+				.map_err(|_| Error::<T>::SomeError)?;
+			// Finding out a way to create a daemon in Substrate will take too much time,
+			// so I decided to keep this synchronous approach.
+			events.cleanup(now);
 
-			<Events<T>>::put(events);
+			events.add_event(event, now);
+			Self::deposit_event(Event::ReceivedEvent(now));
+
+			<OracleEventStorageStorage<T>>::put(events);
 			Ok(())
 		}
 	}
---
+++ pallets/oracle/src/storage.rs
@@ -0,0 +1,117 @@
+
+use frame_support::pallet_prelude::TypeInfo;
+use frame_support::{inherent::Vec};
+use codec::{Encode, Decode};
+/// Timestamp format is intended to be as milliseconds(ms)
+const HOUR_DURATION: u64 = 3_600_000;
+/// Let list size be bound by 1000. This will prevent us from putting too much data
+/// in blockchain storage.
+const LIST_SIZE: u64 = 1000;
+
+pub type RawEvent = Vec<u8>;
+
+#[derive(Clone, Debug, PartialEq, PartialOrd, Default, Encode, Decode, TypeInfo)]
+pub struct OracleEvent {
+    /// just seconds
+    pub timestamp: u64,
+    pub data: Vec<u8>,
+}
+
+/// List-like implementation.
+#[derive(Encode, Decode, TypeInfo)]
+pub struct OracleEventStorage {
+    pub events: Vec<OracleEvent>,
+    /// Current index of start
+    pub start: u64,
+    /// Current amount of valid events
+    pub size: u64,
+    /// Thought of doing some performance enhancement here.
+    #[deprecated]
+    pub last_timestamp: u64,
+}
+
+impl Default for OracleEventStorage {
+    fn default() -> Self {
+        Self::init_sized(LIST_SIZE)
+    }
+}
+impl OracleEventStorage {
+    pub fn init_sized(size: u64) -> Self {
+        let mut v = Vec::new();
+        v.reserve(size as usize);
+        for _i in 0..size {
+            v.push(OracleEvent::default());
+        }
+        Self {
+            events: v,
+            start: 0,
+            size: 0,
+            last_timestamp: 0,
+        }
+    }
+    /// Remove all the outdated events.
+    pub fn cleanup(&mut self, now: u64) {
+        let mut counter = self.get_start();
+        let mut cleaned_start = counter;
+        let vec_size = self.events.len();
+
+        for _i in 0..self.size {
+            let ts = self.events[counter].timestamp;
+            if now - ts > HOUR_DURATION {
+                cleaned_start = (cleaned_start + 1) % vec_size;
+                self.size = self.size - 1;
+            } else {
+                break;
+            }
+            counter = counter + 1;
+        }
+        self.start = cleaned_start as u64;
+    }
+    /// Add a new event. Will rewrite the oldest one if there were more than 1000 events
+    /// in the last hour.
+    pub fn add_event(&mut self, event: Vec<u8>, now: u64) {
+        let event = OracleEvent {
+            timestamp: now,
+            data: event,
+        };
+        let put_idx = (self.get_start() + self.get_size()) % self.events.len();
+        self.events[put_idx] = event;
+        if self.size < self.events.len() as u64 {
+            self.size = self.size + 1;
+        }
+    }
+    pub fn get_start(&self) -> usize {
+        self.start as usize
+    }
+    pub fn get_size(&self) -> usize {
+        self.size as usize
+    }
+}
+
+/// All-in-one to make it easier to run in IDE.
+#[test]
+fn test_storage() {
+    let mut storage = OracleEventStorage::default();
+    assert_eq!(1000, storage.events.len());
+
+    let event = vec![];
+    for _i in 0..50 {
+        storage.add_event(event.clone(), 1000);
+    }
+    assert_eq!(50, storage.size);
+    storage.cleanup(1000 + HOUR_DURATION + 1);
+    assert_eq!(0, storage.size);
+    assert_eq!(50, storage.start);
+
+    for _i in 0..50 {
+        storage.add_event(event.clone(), 1000);
+    }
+    assert_eq!(50, storage.size);
+    for _i in 0..50 {
+        storage.add_event(event.clone(), 1100);
+    }
+    assert_eq!(100, storage.size);
+    storage.cleanup(1000 + HOUR_DURATION + 1);
+    assert_eq!(100, storage.start);
+    assert_eq!(50, storage.size);
+}
